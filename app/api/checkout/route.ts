03c688089353413778bc2a89314da3530a50ea79
Piyush Tyagi <tyagipiyush2233@gmail.com>
2023-08-03 08:58:16 +0530

    fix2

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 // @ts-ignore
 import { validateCartItems } from "use-shopping-cart/utilities"
 
-import { inventory } from "@/config/inventory"
+// import { inventory } from "@/config/inventory"
 import { stripe } from "@/lib/stripe"
 
 export async function POST() {}
